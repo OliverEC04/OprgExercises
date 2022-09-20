2d654d06bca2424ae61c144afc516f2fe1010473
OliverEC <oliverec04@gmail.com>
2022-09-20 22:48:52 +0200

    commit before sadness

--- Exercise3_2/Source.cpp
+++ Exercise3_2/Source.cpp
@@ -3,6 +3,47 @@
 #include <stdlib.h>
 #include <RaspberryDLL.h>
 
+int lightsCountdown[3];
+
+int lightsCheck()
+{
+	for (int light = 0; light < 3; light++)
+	{
+		if (lightsCountdown[light] == 0)
+		{
+			ledOff(light + 4);
+		}
+		else if (lightsCountdown[light] == 1)
+		{
+			ledOn(light + 4);
+		}
+		else
+		{
+			lightsCountdown[light]--;
+		}
+	}
+
+	return 0;
+}
+
+int setLightState(bool green)
+{
+	if (green)
+	{
+		lightsCountdown[0] = 10;
+		lightsCountdown[1] = 1;
+		lightsCountdown[2] = 1;
+	}
+	else
+	{
+		lightsCountdown[0] = 0;
+		lightsCountdown[1] = 1;
+		lightsCountdown[2] = 20;
+	}
+
+	return 0;
+}
+
 int main(void)
 {
 	if (!Open())
@@ -12,8 +53,15 @@ int main(void)
 	}
 
 	printf("Connected to Raspberry Pi\n");
-	
 
+	setLightState(true);
+
+	while (true)
+	{
+
+		lightsCheck();
+		Wait(1);
+	}
 
 	return 0;
 }
